fc874d593ea67eaf9ff966907d59d812b05d93c9
Akhil-2310 <akhilnanavati23@gmail.com>
2024-12-01 02:52:32 +0530

    style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,12 @@ function Hero() {
   return (
     <div className="bg-gray-900 text-white py-20">
       <div className="container mx-auto text-center">
-        <h1 className="text-8xl font-bold mb-4">Welcome to AI agent for Linea</h1>
+        <h1 className="text-8xl font-bold mb-4">Welcome to AI agent for Linea, Base and Mode</h1>
         <p className="text-5xl mb-8">
           Experience the power of AI + Web3
         </p>
         <p className="text-3xl mb-8">
-          Want to do transactions, get info about what consensys does or want to write smart contracts? Do all this in natural language and just one click !!
+          Want to do transactions, get info about web3 or want to write smart contracts? Do all this in natural language and just one click !!
         </p>
         <Link
           to="/chat"
